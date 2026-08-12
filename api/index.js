8f40aa0fdfbd64b2595b15044fde5da547d0d0b7
Bolatan <35336329+Bolatan@users.noreply.github.com>
2026-08-12 03:39:22 +0100

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -1,3 +1,25 @@
-const { app } = require("../server/src/app");
+const { app, connectMongo } = require('../server/src/app');
 
-module.exports = app;
+let initialized = false;
+let initializationPromise = null;
+
+async function initialize() {
+  if (initialized) return;
+
+  if (!initializationPromise) {
+    initializationPromise = connectMongo()
+      .then(() => {
+        initialized = true;
+      })
+      .finally(() => {
+        initializationPromise = null;
+      });
+  }
+
+  await initializationPromise;
+}
+
+module.exports = async (req, res) => {
+  await initialize();
+  return app(req, res);
+};
